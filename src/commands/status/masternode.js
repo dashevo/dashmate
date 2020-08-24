761505be33b6f042ee8ee7698ae5e553b1012572
Leon White <leon.white@dash.org>
2020-08-24 16:37:54 +1200

    feat: use RPC instead of exec in container

--- src/commands/status/masternode.js
+++ src/commands/status/masternode.js
@@ -2,12 +2,14 @@ const { table } = require('table');
 const fetch = require('node-fetch');
 
 const BaseCommand = require('../../oclif/command/BaseCommand');
+const CoreService = require('../../core/CoreService');
 
 class MasternodeStatusCommand extends BaseCommand {
   /**
    * @param {Object} args
    * @param {Object} flags
    * @param {DockerCompose} dockerCompose
+   * @param {CoreService} coreService
    * @param {Config} config
    * @return {Promise<void>}
    */
@@ -15,9 +17,15 @@ class MasternodeStatusCommand extends BaseCommand {
     args,
     flags,
     dockerCompose,
+    createRpcClient,
     config,
   ) {
     const rows = [];
+
+    const coreService = new CoreService(
+      createRpcClient(),
+      dockerCompose.docker.getContainer('core')
+    );
 
     // Version
     const dashdVersion = (await dockerCompose.execCommand(
@@ -26,26 +34,9 @@ class MasternodeStatusCommand extends BaseCommand {
       'dashd --version',
     )).out.split('\n')[0];
 
-    // Sync status
-    const mnsyncStatus = JSON.parse((await dockerCompose.execCommand(
-      config.toEnvs(),
-      'core',
-      'dash-cli mnsync status'
-    )).out);
-
-    // Header and block count
-    const blockchaininfo = JSON.parse((await dockerCompose.execCommand(
-      config.toEnvs(),
-      'core',
-      'dash-cli getblockchaininfo',
-    )).out);
-
-    // Masternode data
-    const masternodeStatus = JSON.parse((await dockerCompose.execCommand(
-      config.toEnvs(),
-      'core',
-      'dash-cli masternode status',
-    )).out);
+    const mnsyncStatus = (await coreService.getRpcClient().mnsync('status')).result;
+    const blockchainInfo = (await coreService.getRpcClient().getBlockchainInfo()).result;
+    const masternodeStatus = (await coreService.getRpcClient().masternode('status')).result;
 
     // Sentinel
     const sentinelState = (await dockerCompose.execCommand(
@@ -54,6 +45,7 @@ class MasternodeStatusCommand extends BaseCommand {
       'python bin/sentinel.py',
     )).out.split('\n')[0];
 
+    // Tendermint
     const tendermintVersion = (await dockerCompose.execCommand(
       config.toEnvs(),
       'drive_tendermint',
@@ -72,22 +64,22 @@ class MasternodeStatusCommand extends BaseCommand {
     // Build table
     rows.push(['Dashd Version', dashdVersion]);
     rows.push(['Sync Status', mnsyncStatus.AssetName]);
-    rows.push(['Headers', blockchaininfo.headers]);
-    rows.push(['Blocks', blockchaininfo.blocks]);
+    rows.push(['Headers', blockchainInfo.headers]);
+    rows.push(['Blocks', blockchainInfo.blocks]);
     rows.push(['Masternode Status', masternodeStatus.state]);
     if (masternodeStatus.state === 'READY') {
       rows.push(['ProTx Hash', masternodeStatus.proTxHash]);
       rows.push(['Service', masternodeStatus.dmnState.service]);
       rows.push(['PoSe Penalty', masternodeStatus.dmnState.PoSePenalty]);
     }
-    rows.push(['Sentinel', (sentinelState !== '' ? sentinelState : 'No errors' )]);
+    rows.push(['Sentinel', (sentinelState !== '' ? sentinelState : 'No errors')]);
     rows.push(['Port Check', config.options.core.p2p.port + ' ' + portState]);
     if (config.network !== 'testnet') {
       rows.push(['Tendermint Version', tendermintVersion]);
       rows.push(['Tendermint Blocks', tendermintStatus.result.sync_info.latest_block_height]);
       rows.push(['Tendermint Sync', !tendermintStatus.result.sync_info.catching_up]);
     }
-    
+
     const output = table(rows, { singleLine: true });
 
     // eslint-disable-next-line no-console
