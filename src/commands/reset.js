1fcafb1c0628e18d908fd6203296c81cfb0d059f
Ivan Shumkov <ivan@shumkov.ru>
2020-08-27 11:30:33 +0300

    feat: remove unnecessary Tendermint unsafe reset

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -4,8 +4,6 @@ const BaseCommand = require('../oclif/command/BaseCommand');
 
 const MuteOneLineError = require('../oclif/errors/MuteOneLineError');
 
-const NETWORKS = require('../networks');
-
 class ResetCommand extends BaseCommand {
   /**
    * @param {Object} args
@@ -25,22 +23,6 @@ class ResetCommand extends BaseCommand {
         title: 'Reset node data',
         task: () => (
           new Listr([
-            {
-              title: 'Cleanup Tendermint data dir',
-              enabled: () => config.get('network') !== NETWORKS.TESTNET,
-              task: async () => {
-                if (await dockerCompose.isServiceRunning(config.toEnvs())) {
-                  throw new Error('You can\'t reset data while MN is running. Please stop it.');
-                }
-
-                await dockerCompose.runService(
-                  config.toEnvs(),
-                  'drive_tendermint',
-                  ['tendermint', 'unsafe_reset_all'],
-                  ['--entrypoint=""'],
-                );
-              },
-            },
             {
               title: 'Remove Docker containers and associated data',
               task: async () => dockerCompose.down(config.toEnvs()),
