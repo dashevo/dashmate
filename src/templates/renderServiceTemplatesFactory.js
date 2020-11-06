7942edc700c08d55d0a4ade6255bb383ffb7024b
Leon White <leon.white@dash.org>
2020-11-05 17:26:16 -0800

    fix: linting

--- src/templates/renderServiceTemplatesFactory.js
+++ src/templates/renderServiceTemplatesFactory.js
@@ -5,7 +5,6 @@ const dots = require('dot');
  * @return {renderServiceTemplates}
  */
 function renderServiceTemplatesFactory(writeServiceConfigs) {
-
   /**
    * Render templates for services
    * @typedef {renderServiceTemplates}
@@ -17,9 +16,9 @@ function renderServiceTemplatesFactory(writeServiceConfigs) {
     const files = fs.readdirSync('./templates');
 
     dots.templateSettings.strip = false;
-    let configFiles = {};
+    const configFiles = {};
     for (const file of files) {
-      const fileContents = fs.readFileSync('./templates/' + file, 'utf-8');
+      const fileContents = fs.readFileSync(`./templates/${file}`, 'utf-8');
       const fileTemplate = dots.template(fileContents);
       if (file === 'genesis.json.template' && Object.keys(config.options.platform.drive.tendermint.genesis).length === 0) {
         continue;
