133068aa35459161aeca325597195ae03df2c133
Leon White <leon.white@dash.org>
2020-11-16 13:31:52 -0800

    fix: refactor to use RPC

--- src/commands/status/masternode.js
+++ src/commands/status/masternode.js
@@ -2,12 +2,14 @@ const { table } = require('table');
 const fetch = require('node-fetch');
 
 const BaseCommand = require('../../oclif/command/BaseCommand');
+const CoreService = require('../../core/CoreService');
 
 class MasternodeStatusCommand extends BaseCommand {
   /**
    * @param {Object} args
    * @param {Object} flags
    * @param {DockerCompose} dockerCompose
+   * @param {CoreService} coreService
    * @param {Config} config
    * @return {Promise<void>}
    */
@@ -15,9 +17,21 @@ class MasternodeStatusCommand extends BaseCommand {
     args,
     flags,
     dockerCompose,
+    createRpcClient,
     config,
   ) {
     const rows = [];
+
+    const coreService = new CoreService(
+      createRpcClient(
+        {
+          port: config.get('core.rpc.port'),
+          user: config.get('core.rpc.user'),
+          pass: config.get('core.rpc.password'),
+        },
+      ),
+      dockerCompose.docker.getContainer('core'),
+    );
 
     // Version
     const dashdVersion = (await dockerCompose.execCommand(
@@ -27,25 +41,13 @@ class MasternodeStatusCommand extends BaseCommand {
     )).out.split('\n')[0];
 
     // Sync status
-    const mnsyncStatus = JSON.parse((await dockerCompose.execCommand(
-      config.toEnvs(),
-      'core',
-      'dash-cli mnsync status'
-    )).out);
+    const mnsyncStatus = (await coreService.getRpcClient().mnsync('status')).result;
 
-    // Header and block count	
-    const blockchainInfo = JSON.parse((await dockerCompose.execCommand(
-      config.toEnvs(),
-      'core',
-      'dash-cli getblockchaininfo',
-    )).out);
+    // Header and block count
+    const blockchainInfo = (await coreService.getRpcClient().getBlockchainInfo()).result;
 
-    // Masternode data	
-    const masternodeStatus = JSON.parse((await dockerCompose.execCommand(
-      config.toEnvs(),
-      'core',
-      'dash-cli masternode status',
-    )).out);
+    // Masternode data
+    const masternodeStatus = (await coreService.getRpcClient().masternode('status')).result;
 
     // Sentinel
     const sentinelState = (await dockerCompose.execCommand(
@@ -76,25 +78,26 @@ class MasternodeStatusCommand extends BaseCommand {
     }
 
     // Port check
-    const corePortState = await fetch('https://mnowatch.org/' + config.options.core.p2p.port + '/').then(res => res.text());
+    const corePortState = await fetch(`https://mnowatch.org/${config.options.core.p2p.port}/`).then((res) => res.text());
 
     if (config.options.network !== 'testnet') {
       const tendermintPortNum = (await dockerCompose.inspectService(
         config.toEnvs(),
-        'drive_tendermint'
+        'drive_tendermint',
       )).NetworkSettings.Ports['26656/tcp'][0].HostPort;
 
-      const tendermintPortState = await fetch('https://mnowatch.org/' + tendermintPortNum + '/').then(res => res.text());
+      const tendermintPortState = await fetch(`https://mnowatch.org/${tendermintPortNum}/`).then((res) => res.text());
     }
-    
+
     // Build table
     rows.push(['Dashd Version', dashdVersion]);
     rows.push(['Sync Status', mnsyncStatus.AssetName]);
-    rows.push(['Core Port', config.options.core.p2p.port + ' ' + corePortState]);
+    rows.push(['Core Port', `${config.options.core.p2p.port} ${corePortState}`]);
     rows.push(['Headers', blockchainInfo.headers]);
     rows.push(['Blocks', blockchainInfo.blocks]);
     rows.push(['Core Sync', mnsyncStatus.IsSynced]);
-    rows.push(['Masternode Status', masternodeStatus.state]);
+    rows.push(['Masternode State', masternodeStatus.state]);
+    rows.push(['Masternode Status', masternodeStatus.status]);
     if (masternodeStatus.state === 'READY') {
       rows.push(['ProTx Hash', masternodeStatus.proTxHash]);
       rows.push(['Service', masternodeStatus.dmnState.service]);
@@ -103,7 +106,7 @@ class MasternodeStatusCommand extends BaseCommand {
     rows.push(['Sentinel', (sentinelState !== '' ? sentinelState : 'No errors')]);
     if (config.options.network !== 'testnet') {
       rows.push(['Tendermint Version', tendermintVersion]);
-      rows.push(['Tendermint Port', tendermintPortNum + ' ' + tendermintPortState]);
+      rows.push(['Tendermint Port', `${tendermintPortNum} ${tendermintPortState}`]);
       if (mnsyncStatus.IsSynced === true) {
         rows.push(['Tendermint Blocks', tendermintStatus.result.sync_info.latest_block_height]);
         rows.push(['Tendermint Sync', !tendermintStatus.result.sync_info.catching_up]);
