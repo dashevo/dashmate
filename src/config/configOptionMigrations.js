e961e37db2ddb0004a901e45ccdb1523dc022cb9
Leon White <leon.white@dash.org>
2020-12-15 16:25:55 -0800

    fix: copy configs from systemConfigs

--- src/config/configOptionMigrations.js
+++ src/config/configOptionMigrations.js
@@ -1,19 +1,28 @@
+const lodashGet = require('lodash.get');
 const lodashSet = require('lodash.set');
 
-// const systemConfigs = require('./systemConfigs/systemConfigs');
+const systemConfigs = require('./systemConfigs/systemConfigs');
 
 module.exports = {
   '0.17.0-dev.12': (name, options) => {
-    lodashSet(options, 'platform.dapi.nginx.rateLimiter.enable', name !== 'base');
-    lodashSet(options, 'platform.dapi.nginx.rateLimiter.rate', 120);
-    lodashSet(options, 'platform.dapi.nginx.rateLimiter.burst', 300);
-
+    // Rename tendermint to tenderdash
     lodashSet(options, 'platform.drive.tenderdash', options.platform.drive.tendermint);
     // eslint-disable-next-line no-param-reassign
     delete options.platform.drive.tendermint;
 
-    lodashSet(options, 'platform.drive.tenderdash.validatorKey', {});
-    lodashSet(options, 'platform.drive.tenderdash.nodeKey', {});
+    // Copy new configs from system defaults
+    const sourceConfigName = name in systemConfigs ? name : 'base';
+    const paths = [
+      'platform.dapi.nginx.rateLimiter.enable',
+      'platform.dapi.nginx.rateLimiter.burst',
+      'platform.dapi.nginx.rateLimiter.rate',
+      'platform.drive.tenderdash.validatorKey',
+      'platform.drive.tenderdash.nodeKey',
+    ];
+
+    paths.forEach((path) => {
+      lodashSet(options, path, lodashGet(systemConfigs[sourceConfigName], path));
+    });
 
     return options;
   },
