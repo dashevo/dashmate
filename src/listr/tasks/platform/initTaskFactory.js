99a6add9dc655d03a9850e63ce92431c1ccf2894
Leon White <leon.white@dash.org>
2020-12-17 14:58:12 -0800

    fix: rename tendermint to tenderdash

--- src/listr/tasks/platform/initTaskFactory.js
+++ src/listr/tasks/platform/initTaskFactory.js
@@ -129,7 +129,7 @@ function initTaskFactory(
           const response = await tenderdashRpcClient.request('tx', params);
 
           if (response.error) {
-            throw new Error(`Tendermint error: ${response.error.message}: ${response.error.data}`);
+            throw new Error(`Tenderdash error: ${response.error.message}: ${response.error.data}`);
           }
 
           const { result: { height: contractBlockHeight } } = response;
