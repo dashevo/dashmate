83fafa805f5f5e4031c75393f975c07336a4d9c4
Leon White <leon.white@dash.org>
2020-12-24 21:40:05 -0800

    feat: specify config to setup as args

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -1,5 +1,8 @@
 const { Listr } = require('listr2');
 const publicIp = require('public-ip');
+
+const { flags: flagTypes } = require('@oclif/command');
+
 const NETWORKS = require('../networks');
 
 const BaseCommand = require('../oclif/command/BaseCommand');
@@ -19,7 +22,11 @@ class SetupCommand extends BaseCommand {
    */
   async runWithDependencies(
     args,
-    flags,
+    {
+      'external-ip': externalIp,
+      'operator-bls-private-key': operatorBlsPrivateKey,
+      network,
+    },
     dockerCompose,
     generateBlsKeys,
     configCollection,
@@ -30,16 +37,20 @@ class SetupCommand extends BaseCommand {
       {
         title: 'Select network',
         task: async (ctx, task) => {
-          const input = await task.prompt([
-            {
-              type: 'select',
-              name: 'network',
-              message: 'Select network to configure',
-              choices: [...Object.values(NETWORKS)],
-              initial: 'testnet',
-            },
-          ]);
-          ctx.network = input;
+          if (network === null) {
+            const input = await task.prompt([
+              {
+                type: 'select',
+                name: 'network',
+                message: 'Select network to configure',
+                choices: [...Object.values(NETWORKS)],
+                initial: 'testnet',
+              },
+            ]);
+            ctx.network = input;
+          } else {
+            ctx.network = network;
+          }
           configCollection.setDefaultConfigName(ctx.network);
           // eslint-disable-next-line no-param-reassign
           task.output = `Selected ${configCollection.getDefaultConfigName()} config\n`;
@@ -51,13 +62,8 @@ class SetupCommand extends BaseCommand {
         options: { persistentOutput: true },
       },
       {
-        title: 'Collect data',
-        task: async (ctx) => {
-          ctx.isGenesisEmpty = Object.keys(config.get('platform.drive.tenderdash.genesis')).length === 0;
-        },
-      },
-      {
         title: 'Configure external IP address',
+        enabled: () => externalIp === null,
         task: async (ctx, task) => {
           const input = await task.prompt([
             {
@@ -71,7 +77,18 @@ class SetupCommand extends BaseCommand {
         },
       },
       {
+        title: 'Configure external IP address',
+        enabled: () => externalIp !== null,
+        task: async (ctx, task) => {
+          ctx.externalIp = externalIp;
+          // eslint-disable-next-line no-param-reassign
+          task.output = `Using ${externalIp}`;
+        },
+        options: { persistentOutput: true },
+      },
+      {
         title: 'Configure BLS private key',
+        enabled: () => operatorBlsPrivateKey === null,
         task: async (ctx, task) => {
           const generatedBls = await generateBlsKeys();
           const input = await task.prompt([
@@ -91,12 +108,24 @@ class SetupCommand extends BaseCommand {
         options: { persistentOutput: true },
       },
       {
+        title: 'Configure BLS private key',
+        enabled: () => operatorBlsPrivateKey !== null,
+        task: async (ctx, task) => {
+          ctx.blsPrivKey = operatorBlsPrivateKey;
+          // eslint-disable-next-line no-param-reassign
+          task.output = `Using ${operatorBlsPrivateKey.slice(0, 8)}...`;
+        },
+        options: { persistentOutput: true },
+      },
+      {
         title: 'Initialize Tenderdash',
         task: async (ctx) => {
           const [validatorKey, nodeKey, genesis] = await initializeTenderdashNode(config);
           ctx.validatorKey = validatorKey;
           ctx.nodeKey = nodeKey;
+
           // Don't overwrite existing genesis in config
+          ctx.isGenesisEmpty = Object.keys(config.get('platform.drive.tenderdash.genesis')).length === 0;
           ctx.genesis = ctx.isGenesisEmpty ? genesis : config.get('platform.drive.tenderdash.genesis');
         },
       },
@@ -134,6 +163,9 @@ Set up node config
 
 SetupCommand.flags = {
   ...BaseCommand.flags,
+  network: flagTypes.string({ char: 'n', description: 'network name', default: null }),
+  'external-ip': flagTypes.string({ char: 'i', description: 'external ip', default: null }),
+  'operator-bls-private-key': flagTypes.string({ char: 'k', description: 'operator bls private key', default: null }),
 };
 
 module.exports = SetupCommand;
