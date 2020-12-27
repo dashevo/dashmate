31fea574db279f4c1ca54cac08fefd8f6c64929f
Leon White <leon.white@dash.org>
2020-12-27 15:18:53 -0800

    fix: set core docker image in base instead of testnet

--- src/config/systemConfigs/systemConfigs.js
+++ src/config/systemConfigs/systemConfigs.js
@@ -6,7 +6,7 @@ const baseConfig = {
   description: 'base config for use as template',
   core: {
     docker: {
-      image: 'dashpay/dashd-develop:latest',
+      image: 'dashpay/dashd:0.17.0.0-rc2',
     },
     p2p: {
       port: 20001,
@@ -915,9 +915,6 @@ module.exports = {
   testnet: lodashMerge({}, baseConfig, {
     description: 'node with testnet configuration',
     core: {
-      docker: {
-        image: 'dashpay/dashd:0.17.0.0-rc2',
-      },
       p2p: {
         port: 19999,
       },
