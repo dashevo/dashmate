41679ab2d2d0c3c8f05a9244f43e63da904d16d4
Ivan Shumkov <ivan@shumkov.ru>
2021-01-13 08:25:27 +0800

    feat: add seed nodes for testnet (#239)

--- src/config/configOptionMigrations.js
+++ src/config/configOptionMigrations.js
@@ -1,28 +1,12 @@
-const lodashGet = require('lodash.get');
 const lodashSet = require('lodash.set');
 
 const systemConfigs = require('./systemConfigs/systemConfigs');
 
 module.exports = {
-  '0.17.0-dev.12': (name, options) => {
-    // Rename tendermint to tenderdash
-    lodashSet(options, 'platform.drive.tenderdash', options.platform.drive.tendermint);
-    // eslint-disable-next-line no-param-reassign
-    delete options.platform.drive.tendermint;
-
-    // Copy new configs from system defaults
-    const sourceConfigName = name in systemConfigs ? name : 'base';
-    const paths = [
-      'platform.dapi.nginx.rateLimiter.enable',
-      'platform.dapi.nginx.rateLimiter.burst',
-      'platform.dapi.nginx.rateLimiter.rate',
-      'platform.drive.tenderdash.validatorKey',
-      'platform.drive.tenderdash.nodeKey',
-    ];
-
-    paths.forEach((path) => {
-      lodashSet(options, path, lodashGet(systemConfigs[sourceConfigName], path));
-    });
+  '0.17.2': (name, options) => {
+    // Set seed nodes for tenderdash
+    lodashSet(options, 'platform.drive.tenderdash.p2p.seeds', systemConfigs.testnet.platform.drive.tenderdash.p2p.seeds);
+    lodashSet(options, 'platform.drive.tenderdash.p2p.persistentPeers', []);
 
     return options;
   },
