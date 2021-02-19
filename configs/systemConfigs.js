5cb4ddb592b1e2f69fccb03a9da249b4758ce3ed
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-02-19 16:56:26 +0300

    merge with 0.19-dev

--- configs/systemConfigs.js
+++ configs/systemConfigs.js
@@ -1,11 +1,9 @@
 const baseConfig = require('./system/base');
 const localConfig = require('./system/local');
-const devnetConfig = require('./system/devnet');
 const testnetConfig = require('./system/testnet');
 
 module.exports = {
   base: baseConfig,
   local: localConfig,
-  devnet: devnetConfig,
   testnet: testnetConfig,
 };
