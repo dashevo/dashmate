20ccd0b9788b5d020deafc076676b41806691d4d
Ivan Shumkov <ivan@shumkov.ru>
2021-02-25 14:44:44 +0800

    docs: add todo

--- src/commands/group/reset.js
+++ src/commands/group/reset.js
@@ -54,6 +54,8 @@ class GroupResetCommand extends GroupBaseCommand {
 
               config.set('platform.dpns', baseConfig.platform.dpns);
               config.set('platform.dashpay', baseConfig.platform.dashpay);
+
+              // TODO: Should stay the same
               config.set('platform.drive.tenderdash.nodeId', baseConfig.platform.drive.tenderdash.nodeId);
               config.set('platform.drive.tenderdash.validatorKey', baseConfig.platform.drive.tenderdash.validatorKey);
               config.set('platform.drive.tenderdash.nodeKey', baseConfig.platform.drive.tenderdash.nodeKey);
