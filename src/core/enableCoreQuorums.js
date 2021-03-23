9fb293c30c4b3e282a93bbaf64188972062bb67e
Anton Suprunchuk <antouhou@gmail.com>
2021-03-23 18:15:08 +0300

    add waitForMasternodeProbes functions

--- src/core/enableCoreQuorums.js
+++ src/core/enableCoreQuorums.js
@@ -86,7 +86,7 @@ async function mineQuorum(rpcClient) {
   const isSpork21Active = sporks['SPORK_21_QUORUM_ALL_CONNECTED'] === 0;
 
   if (isSpork21Active) {
-    await waitForMasternodeProbes(mninfos, wait_proc=lambda: self.bump_mocktime(1, nodes=nodes));
+    await waitForMasternodeProbes(mninfos);
   }
 
   await bumpMockTimeForNodes(1, nodes);
@@ -196,6 +196,7 @@ async function waitForQuorumPhase(rpcClients, quorumHash, phase, expectedMemberC
   const timeOut = Date.now() + timeout;
   let isReady = false;
   let dkgSessionIsOk = false;
+
   while (isReady) {
     await wait(checkInterval);
     const dkgSessionIsOk = await checkDKGSessionPhase(rpcClients, quorumHash, phase, expectedMemberCount, checkReceivedMessages, checkReceivedMessagesCount);
@@ -342,14 +343,14 @@ async function checkQuorumConnections(rpcClients, expectedConnectionsCount) {
   let allOk = true;
 
   for (const rpc of rpcClients) {
-    const { result: status } = await rpc.quorum('dkgstatus');
+    const { result: dkgStatus } = await rpc.quorum('dkgstatus');
 
-    if (Object.keys(status.session).length === 0) {
+    if (Object.keys(dkgStatus.session).length === 0) {
       continue;
     }
 
-    const noConnections = status.quorumConnections == null;
-    const llmqConnections = status.quorumConnections;
+    const noConnections = dkgStatus.quorumConnections == null;
+    const llmqConnections = dkgStatus.quorumConnections;
     const noLlmqTestConnections = noConnections || llmqConnections['llmq_test'] == null;
 
     if (noLlmqTestConnections) {
@@ -403,15 +404,85 @@ async function waitForQuorumConnections(expectedConnectionsCount, rpcClients, ti
   return isOk;
 }
 
+async function waitProbes(rpcClients) {
+  await bumpMockTimeForNodes(1, rpcClients);
+  return false;
+}
+
+/**
+ *
+ * @param {RpcClient[]} rpcClients
+ * @return {Promise<boolean>}
+ */
+async function checkProbes(rpcClients) {
+
+  for (const rpc of rpcClients) {
+    const dkgStatus = rpc.quorum('dkgstatus');
+    const { session, quorumConnections } = dkgStatus;
+
+    if (Object.keys(session).length === 0) {
+      continue;
+    }
+
+    if (!quorumConnections || !quorumConnections['llmq_test']) {
+      return await waitProbes();
+    }
+
+    for (let connection of quorumConnections['llmq_test']) {
+      // TODO: if connection is the current rpc node, skip
+      if (!connection.outbound) {
+        const mnInfo = await rpc.protx(connection.proTxHash);
+
+        for (masternode in mnInfos) {
+          if (connection.protxHash === masternode.protxHash) {
+            // MN is expected to be online and functioning, so let's verify that the last successful
+            // probe is not too old. Probes are retried after 50 minutes, while DKGs consider a probe
+            // as failed after 60 minutes
+            if (mnInfo['metaInfo']['lastOutboundSuccessElapsed'] > 55 * 60) {
+              return await waitProbes();
+            }
+          } else {
+            // MN is expected to be offline, so let's only check that the last probe is not too long ago
+            if (mnInfo['metaInfo']['lastOutboundAttemptElapsed'] > 55 * 60 && mnInfo['metaInfo']['lastOutboundSuccessElapsed'] > 55 * 60) {
+              return await waitProbes();
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+/**
+ *
+ * @param {RpcClient[]} rpcClients
+ * @param {number} [timeout]
+ * @return {Promise<boolean>}
+ */
+async function waitForMasternodeProbes(rpcClients, timeout = 30000) {
+  let isReady = false;
+  let isOk = false;
+  const deadline = Date.now() + timeout;
+
+  while (!isReady) {
+    isOk = await checkProbes(rpcClients);
+    isReady = isOk;
+
+    if (Date.now() > deadline) {
+      isReady = true;
+    }
+  }
+
+  return isOk;
+}
+
 async function checkDKGSessionCommitments() {
 
 }
 
 async function waitForQuorumCommitments() {
-
-}
-
-async function waitForMasternodeProbes() {
 
 }
 
