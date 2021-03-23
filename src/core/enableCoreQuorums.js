39d0d20dbda72ce2cfb8ee6e6876607aeb33f636
Anton Suprunchuk <antouhou@gmail.com>
2021-03-23 18:35:13 +0300

    add waitForQuorumCommitments functions

--- src/core/enableCoreQuorums.js
+++ src/core/enableCoreQuorums.js
@@ -433,6 +433,7 @@ async function checkProbes(rpcClients) {
       if (!connection.outbound) {
         const mnInfo = await rpc.protx(connection.proTxHash);
 
+        // TODO: add mnInfos to the functions
         for (masternode in mnInfos) {
           if (connection.protxHash === masternode.protxHash) {
             // MN is expected to be online and functioning, so let's verify that the last successful
@@ -478,12 +479,67 @@ async function waitForMasternodeProbes(rpcClients, timeout = 30000) {
   return isOk;
 }
 
-async function checkDKGSessionCommitments() {
+/**
+ *
+ * @param {string} quorumHash
+ * @param {RpcClient[]} rpcClients
+ * @return {Promise<boolean>}
+ */
+async function checkDKGSessionCommitments(quorumHash, rpcClients) {
+  // This one doesn't have a wait stage
 
+  let allOk = true;
+
+  for (const rpc of rpcClients) {
+    const { result: dkgStatus } = rpc.quorum('dkgstatus');
+
+    if (!dkgStatus.minableCommitments) {
+      allOk = false;
+      break;
+    }
+
+    const commitments = dkgStatus.minableCommitments;
+
+    if (!commitments["llmq_test"]) {
+      allOk = false;
+      break;
+    }
+
+    const quorum = commitments["llmq_test"];
+    if (quorum.quorumHash !== quorumHash) {
+      allOk = false;
+      break;
+    }
+  }
+
+  return allOk;
 }
 
-async function waitForQuorumCommitments() {
+/**
+ *
+ * @param {string} quorumHash
+ * @param {RpcClient[]} rpcClients
+ * @param {number} [timeout]
+ * @param {number} [waitBeforeRetry]
+ * @return {Promise<boolean>}
+ */
+async function waitForQuorumCommitments(quorumHash, rpcClients, timeout = 15000, waitBeforeRetry = 100) {
+  let isReady = false;
+  let isOk = false;
+  const deadline = Date.now() + timeout;
 
+  while (!isReady) {
+    await wait(waitBeforeRetry);
+
+    isOk = await checkDKGSessionCommitments(rpcClients);
+    isReady = isOk;
+
+    if (Date.now() > deadline) {
+      isReady = true;
+    }
+  }
+
+  return isOk;
 }
 
 module.exports = enableCoreQuorums;
