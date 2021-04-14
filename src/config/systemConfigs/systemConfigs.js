a0bb778952c5c83bcc5b69cc5c124b57b8c25b02
strophy <32928115+strophy@users.noreply.github.com>
2021-04-15 00:07:46 +0200

    feat: update to core 0.17.0.0-rc4 (#326)

--- src/config/systemConfigs/systemConfigs.js
+++ src/config/systemConfigs/systemConfigs.js
@@ -6,7 +6,7 @@ const baseConfig = {
   description: 'base config for use as template',
   core: {
     docker: {
-      image: 'dashpay/dashd:0.17.0.0-rc3-hotfix1',
+      image: 'dashpay/dashd:0.17.0.0-rc4',
     },
     p2p: {
       port: 20001,
