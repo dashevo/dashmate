2cc6738daf9887c0143c007b434b7dd18a29c05e
strophy <badfunkstripe@gmail.com>
2021-05-03 12:31:16 +0200

    fix: config with name 'devnet' is not present

--- configs/system/index.js
+++ configs/system/index.js
@@ -1,9 +1,11 @@
 const baseConfig = require('./base');
 const localConfig = require('./local');
 const testnetConfig = require('./testnet');
+const devnetConfig = require('./devnet');
 
 module.exports = {
   base: baseConfig,
   local: localConfig,
   testnet: testnetConfig,
+  devnet: devnetConfig,
 };
