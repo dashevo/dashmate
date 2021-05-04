3ecf60194868462f81c53b558bfc702a1f180bfe
Ivan Shumkov <ivan@shumkov.ru>
2021-05-04 13:43:20 +0300

    revert: drop devnet config

--- configs/system/index.js
+++ configs/system/index.js
@@ -1,11 +1,9 @@
 const baseConfig = require('./base');
 const localConfig = require('./local');
 const testnetConfig = require('./testnet');
-const devnetConfig = require('./devnet');
 
 module.exports = {
   base: baseConfig,
   local: localConfig,
   testnet: testnetConfig,
-  devnet: devnetConfig,
 };
