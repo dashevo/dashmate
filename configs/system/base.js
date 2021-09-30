2aec8de9b0ede4cdafd685152e9323b95c143ba4
strophy <badfunkstripe@gmail.com>
2021-09-30 18:19:55 +0200

    fix: incorrect path.join() notation

--- configs/system/base.js
+++ configs/system/base.js
@@ -91,11 +91,11 @@ module.exports = {
           },
           prettyFile: {
             level: 'silent',
-            path: path.join(HOME_DIR_PATH, '/base/logs/', '/drive-pretty.log'),
+            path: path.join(HOME_DIR_PATH, 'base', 'logs', 'drive-pretty.log'),
           },
           jsonFile: {
             level: 'silent',
-            path: path.join(HOME_DIR_PATH, '/base/logs/', '/drive-json.log'),
+            path: path.join(HOME_DIR_PATH, 'base', 'logs', 'drive-json.log'),
           },
         },
         validatorSet: {
