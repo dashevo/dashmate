4d9526b78bfef41fd6b00c273e8a22dcc83fd4a4
Daniel <daniel@ktechmidas.net>
2021-11-02 09:30:15 +0000

    host json output

--- src/commands/status/host.js
+++ src/commands/status/host.js
@@ -14,29 +14,44 @@ class HostStatusCommand extends ConfigBaseCommand {
   /**
    * @return {Promise<void>}
    */
-  async runWithDependencies(flags) {
+  async runWithDependencies(args, flags) {
     const rows = [];
 
-    rows.push(['Hostname', os.hostname()]);
-    rows.push(['Uptime', prettyMs(os.uptime() * 1000)]);
-    rows.push(['Platform', os.platform()]);
-    rows.push(['Arch', os.arch()]);
-    rows.push(['Username', os.userInfo().username]);
-    rows.push(['Loadavg', os.loadavg().map((load) => load.toFixed(2))]);
-    rows.push(['Diskfree', 0]); // Waiting for feature: https://github.com/nodejs/node/pull/31351
-    rows.push(['Memory', `${prettyByte(os.totalmem())} / ${prettyByte(os.freemem())}`]);
-    rows.push(['CPUs', os.cpus().length]);
-    rows.push(['IP', await publicIp.v4()]);
+    const outputRows = {
+      Hostname: os.hostname(),
+      Uptime: prettyMs(os.uptime() * 1000),
+      Platform: os.platform(),
+      Arch: os.arch(),
+      Username: os.userInfo().username,
+      Diskfree: 0,
+      Memory: `${prettyByte(os.totalmem())} / ${prettyByte(os.freemem())}`,
+      CPUs: os.cpus().length,
+      IP: await publicIp.v4()
+    }
+
+    //rows.push(['Hostname', os.hostname()]);
+    //rows.push(['Uptime', prettyMs(os.uptime() * 1000)]);
+    //rows.push(['Platform', os.platform()]);
+    //rows.push(['Arch', os.arch()]);
+    //rows.push(['Username', os.userInfo().username]);
+    //rows.push(['Loadavg', os.loadavg().map((load) => load.toFixed(2))]);
+    //rows.push(['Diskfree', 0]); // Waiting for feature: https://github.com/nodejs/node/pull/31351
+    //rows.push(['Memory', `${prettyByte(os.totalmem())} / ${prettyByte(os.freemem())}`]);
+    //rows.push(['CPUs', os.cpus().length]);
+    //rows.push(['IP', await publicIp.v4()]);
     
     const outputFormat = getFormat(flags);
-    console.log(flags);
+    //console.log(flags);
     let output;
     
     if (outputFormat === OUTPUT_FORMATS.JSON) {
       output = stripAnsi(JSON.stringify(outputRows));
     }
     else {
-    output = table(rows, { singleLine: true });
+      Object.keys(outputRows).forEach((key) => {
+        rows.push([key, outputRows[key]]);
+      });
+      output = table(rows, { singleLine: true });
     }
     // eslint-disable-next-line no-console
     console.log(output);
